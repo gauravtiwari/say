d2f6b77c2ac9de69f5a6dafde26885e5dbed970c
Gaurav Tiwari <gaurav@gauravtiwari.co.uk>
2015-10-24 21:39:30 +0100

    Add post list component and load it

--- app/frontend/index.js
+++ app/frontend/index.js
@@ -1,6 +1,8 @@
 window.React = require('react');
 
 import HelloWorld from './components/hello-world';
+import PostList from './components/post-list';
 
 registerComponent('hello-world', HelloWorld);
+registerComponent('post-list', PostList);
 
