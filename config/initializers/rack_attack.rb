966e32045aec2dc111353f3dad21b2234e3bcc36
Gaurav Tiwari <gaurav@gauravtiwari.co.uk>
2015-10-24 17:43:07 +0100

    Setup rack attack

--- config/initializers/rack_attack.rb
+++ config/initializers/rack_attack.rb
@@ -1,14 +1,15 @@
-break unless Rails.env.production?
-  class Rack::Attack
-    # Throttle all requests by IP (60rpm)
-    throttle('req/ip', :limit => 600, :period => 1.minute) do |req|
-      req.ip unless req.path.starts_with?('/assets')
-    end
-
+class Rack::Attack
+  # Throttle all requests by IP (60rpm)
+  # Key: "rack::attack:#{Time.now.to_i/:period}:req/ip:#{req.ip}"
+  throttle('req/ip', :limit => 1000, :period => 1.minute) do |req|
+    req.ip unless req.path.starts_with?('/assets')
   end
 
-  # Configure app to use Rack::Attack in production
-  Rails.application.configure do
-    # Use only in production
-    config.middleware.use Rack::Attack
-  end
+end
+
+# Configure app to use Rack::Attack in production
+Rails.application.configure do
+  # Use only in production
+  break unless Rails.env.production?
+  config.middleware.use Rack::Attack
+end
