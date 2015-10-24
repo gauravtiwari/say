2b8e138ce21cd5a4c76036f5985287dad714de09
Gaurav Tiwari <gaurav@gauravtiwari.co.uk>
2015-10-24 17:21:53 +0100

    Setup secure headers in production

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,24 @@
 class ApplicationController < ActionController::API
+
+  before_filter :setup_headers, if: :production?
+
+  private
+
+    def setup_headers
+      # Setup Secure header
+      ensure_security_headers(
+        :hsts             => {:max_age => 631138519, :include_subdomains => false},
+        :x_xss_protection => {:value => 1, :mode => 'block'},
+        :x_content_type_options => {:value => 'nosniff'},
+        :x_download_options => {:value => 'noopen'},
+        :x_permitted_cross_domain_policies => {:value => 'none'},
+        :x_frame_options => 'DENY',
+        :csp => false
+      )
+    end
+
+    def production?
+      Rails.env.production?
+    end
+
 end
