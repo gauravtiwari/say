7f62346853adea3f335e3fb2db394c3569c4a627
Gaurav Tiwari <gaurav@gauravtiwari.co.uk>
2015-10-24 21:38:23 +0100

    Refactor post attributes

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,10 +1,8 @@
 class Post < Ohm::Model
 
-  attribute :token
   attribute :timestamp
   attribute :user_id
   attribute :user_name
-  attribute :command
   attribute :text
-  attribute :trigger_word
+
 end
