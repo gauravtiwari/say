581aa71375af961385828963d0e038d7d015fcca
Gaurav Tiwari <gaurav@gauravtiwari.co.uk>
2015-10-24 18:40:45 +0100

    Setup a basic slack responder

--- app/lib/slack/response.rb
+++ app/lib/slack/response.rb
@@ -1,3 +1,18 @@
-class Response
+class Slack::Response
 
+  def initialize(message)
+    @message = message
+  end
+
+  def respond?
+    response.present?
+  end
+
+  def response
+    @response ||= "You asked: #{message}"
+  end
+
+  private
+
+  attr_reader :message
 end
