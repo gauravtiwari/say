5555f863f3a7a52213ee48ed2545a252c118e941
Gaurav Tiwari <gaurav@gauravtiwari.co.uk>
2015-10-24 18:40:35 +0100

    Rename model name to post

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,9 +1,10 @@
-class Say < Ohm::Model
+class Post < Ohm::Model
 
   attribute :token
   attribute :timestamp
   attribute :user_id
   attribute :user_name
+  attribute :command
   attribute :text
   attribute :trigger_word
 end
