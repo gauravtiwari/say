bcbf0639201aaf840b75060cb17dd850ef1e1ac8
Gaurav Tiwari <gaurav@gauravtiwari.co.uk>
2015-10-25 11:19:06 +0000

    Refactor posts controller

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,63 +1,41 @@
 class PostsController < ApplicationController
-  before_action :set_post, only: [:show, :edit, :update, :destroy]
-  skip_before_filter :verify_authenticity_token, only: [:create, :update]
+  skip_before_filter :verify_authenticity_token
   before_filter :verify_slack_token, except: [:index, :show]
 
   # GET /posts
   def index
-    @posts = Post.all.to_a
-
+    @posts = Post.all
     respond_to do |format|
       format.html
-      format.json {render json: @posts}
     end
   end
 
-  # GET /posts/1
-  def show
+  # GET /posts/user
+ def user
+  if params[:command].present?
+    @posts = Post.where(user_id: params[:user_id]).order(id: :desc)
+    render text: "*Here are your past quotes:* \n" + @posts.map{
+      |p|  "#{p.id}. " + p.text.delete("\n")
+    }.join("\n")
+  else
+    render nothing: true, status: :forbidden
   end
-
-  # GET /posts/new
-  def new
-    @post = Post.new
-  end
-
-  # GET /posts/1/edit
-  def edit
-  end
+ end
 
   # POST /posts
   def create
     render nothing: true, status: :ok and return unless responder.respond?
-
-    if params[:command].present?
-      @post = Post.create(post_params)
+    @post = Post.new(post_params)
+    if params[:command].present? && @post.save
+      AddBackgroundJob.perform_later(@post.id)
       render text: responder.response.to_s
     else
       render json: { text: responder.response.to_s }
     end
-  end
 
-  # PATCH/PUT /posts/1
-  def update
-    if @post.update(post_params)
-      redirect_to @post, notice: 'Post was successfully updated.'
-    else
-      render :edit
-    end
-  end
-
-  # DELETE /posts/1
-  def destroy
-    @post.destroy
-    redirect_to posts_url, notice: 'Post was successfully destroyed.'
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_post
-      @post = Post.find(params[:id])
-    end
 
     # Only allow a trusted parameter "white list" through.
     def post_params
