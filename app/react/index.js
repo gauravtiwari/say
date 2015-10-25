0ffad279dcffbdd1c4583204ecb3fe1840a12507
Gaurav Tiwari <gaurav@gauravtiwari.co.uk>
2015-10-25 11:18:46 +0000

    Delete react components

--- app/react/index.js
+++
@@ -1,8 +0,0 @@
-window.React = require('react');
-
-import HelloWorld from './components/hello-world';
-import PostList from './components/post-list';
-
-registerComponent('hello-world', HelloWorld);
-registerComponent('post-list', PostList);
-
