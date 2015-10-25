c84fdef20e788e2136b25fece42f29535d777fb5
Gaurav Tiwari <gaurav@gauravtiwari.co.uk>
2015-10-25 13:03:01 +0000

    Show random quotes

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -4,7 +4,7 @@ class PostsController < ApplicationController
 
   # GET /posts
   def index
-    @posts = Post.all
+    @posts = Post.all.order("RANDOM()")
     respond_to do |format|
       format.html
     end
