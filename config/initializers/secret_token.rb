282ebdd46e818e8c5c79443ef5fbcf08782a0c09
Gaurav Tiwari <gaurav@gauravtiwari.co.uk>
2015-10-25 11:30:48 +0000

    Setup secret token through ENV

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -10,10 +10,10 @@
 # Make sure your secret_key_base is kept private
 # if you're sharing your code publicly.
 
-# Although this is not needed for an api-only application, rails4 
-# requires secret_key_base or secret_token to be defined, otherwise an 
+# Although this is not needed for an api-only application, rails4
+# requires secret_key_base or secret_token to be defined, otherwise an
 # error is raised.
 # Using secret_token for rails3 compatibility. Change to secret_key_base
 # to avoid deprecation warning.
 # Can be safely removed in a rails3 api-only application.
-TeamSays::Application.config.secret_token = '6ccf5fc85dff3d31b758de4d381b93f902114c87709d7cfdc855692e1c318048ac276ce32c5da26032307fac5284762a9f498383d0a1f756293884dd3f7a8139'
+TeamSays::Application.config.secret_token = ENV['SECRET_KEY_BASE']
