2c6bc2daa9f72489d3ffd8b18943804b594b749c
Gaurav Tiwari <gaurav@gauravtiwari.co.uk>
2015-10-25 11:19:17 +0000

    Add transactional callbacks for AR

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -46,6 +46,8 @@ Rails.application.configure do
   # yet still be able to expire them through the digest params.
   config.assets.digest = true
 
+  config.active_record.raise_in_transactional_callbacks = true
+
   # `config.assets.precompile` and `config.assets.version` have moved to config/initializers/assets.rb
 
   # Specifies the header that your server uses for sending files.
