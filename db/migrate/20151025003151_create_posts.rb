b454cdae06489ac799cecf3830a105b13de2b4ce
Gaurav Tiwari <gaurav@gauravtiwari.co.uk>
2015-10-25 14:05:19 +0000

    Add validation to database

--- db/migrate/20151025003151_create_posts.rb
+++ db/migrate/20151025003151_create_posts.rb
@@ -1,10 +1,10 @@
 class CreatePosts < ActiveRecord::Migration
   def change
     create_table :posts do |t|
-      t.integer :user_id, index: true
+      t.integer :user_id, index: true, null: false
       t.string :background
-      t.string :user_name, index: true
-      t.text :text
+      t.string :user_name, index: true, null: false
+      t.text :text, null: false
 
       t.timestamps null: false
     end
